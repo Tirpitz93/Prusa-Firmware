eed2ec22159b09a4947534ed51f54bc9ed7d9338
Tirpitz93 <LoenjaSelter@gmail.com>
2025-06-14 17:19:11 +0100

    add more nozzle diameters

--- Firmware/ultralcd.cpp
+++ Firmware/ultralcd.cpp
@@ -4276,40 +4276,59 @@ while (0)
 static void lcd_nozzle_diameter_cycle(void) {
     uint16_t nDiameter;
     switch(oNozzleDiameter){
+    case ClNozzleDiameter::_Diameter_150:
+        oNozzleDiameter = ClNozzleDiameter::_Diameter_250;
+        nDiameter = 250;
+        break;
     case ClNozzleDiameter::_Diameter_250:
-        oNozzleDiameter=ClNozzleDiameter::_Diameter_400;
-        nDiameter=400;
+        oNozzleDiameter = ClNozzleDiameter::_Diameter_400;
+        nDiameter = 400;
         break;
     case ClNozzleDiameter::_Diameter_400:
-        oNozzleDiameter=ClNozzleDiameter::_Diameter_600;
-        nDiameter=600;
+        oNozzleDiameter = ClNozzleDiameter::_Diameter_600;
+        nDiameter = 600;
         break;
     case ClNozzleDiameter::_Diameter_600:
-        oNozzleDiameter=ClNozzleDiameter::_Diameter_800;
-        nDiameter=800;
+        oNozzleDiameter = ClNozzleDiameter::_Diameter_800;
+        nDiameter = 800;
         break;
     case ClNozzleDiameter::_Diameter_800:
-        oNozzleDiameter=ClNozzleDiameter::_Diameter_250;
-        nDiameter=250;
+        oNozzleDiameter = ClNozzleDiameter::_Diameter_1000;
+        nDiameter = 1000;
+        break;
+    case ClNozzleDiameter::_Diameter_1000:
+        oNozzleDiameter = ClNozzleDiameter::_Diameter_1200;
+        nDiameter = 1200;
+        break;
+    case ClNozzleDiameter::_Diameter_1200:
+        oNozzleDiameter = ClNozzleDiameter::_Diameter_1400;
+        nDiameter = 1400;
+        break;
+    case ClNozzleDiameter::_Diameter_1400:
+        oNozzleDiameter = ClNozzleDiameter::_Diameter_150;
+        nDiameter = 150;
         break;
     default:
-        oNozzleDiameter=ClNozzleDiameter::_Diameter_400;
-        nDiameter=400;
+        oNozzleDiameter = ClNozzleDiameter::_Diameter_400;
+        nDiameter = 400;
     }
-    eeprom_update_byte_notify((uint8_t*)EEPROM_NOZZLE_DIAMETER,(uint8_t)oNozzleDiameter);
-    eeprom_update_word_notify((uint16_t*)EEPROM_NOZZLE_DIAMETER_uM,nDiameter);
+    eeprom_update_byte_notify((uint8_t*)EEPROM_NOZZLE_DIAMETER, (uint8_t)oNozzleDiameter);
+    eeprom_update_word_notify((uint16_t*)EEPROM_NOZZLE_DIAMETER_uM, nDiameter);
 }
-
 #define SETTINGS_NOZZLE \
 do\
 {\
     switch(oNozzleDiameter)\
     {\
+        case ClNozzleDiameter::_Diameter_150: MENU_ITEM_TOGGLE_P(_T(MSG_NOZZLE_DIAMETER), PSTR("0.15"), lcd_nozzle_diameter_cycle); break;\
         case ClNozzleDiameter::_Diameter_250: MENU_ITEM_TOGGLE_P(_T(MSG_NOZZLE_DIAMETER), PSTR("0.25"), lcd_nozzle_diameter_cycle); break;\
         case ClNozzleDiameter::_Diameter_Undef: \
         case ClNozzleDiameter::_Diameter_400: MENU_ITEM_TOGGLE_P(_T(MSG_NOZZLE_DIAMETER), PSTR("0.40"), lcd_nozzle_diameter_cycle); break;\
         case ClNozzleDiameter::_Diameter_600: MENU_ITEM_TOGGLE_P(_T(MSG_NOZZLE_DIAMETER), PSTR("0.60"), lcd_nozzle_diameter_cycle); break;\
         case ClNozzleDiameter::_Diameter_800: MENU_ITEM_TOGGLE_P(_T(MSG_NOZZLE_DIAMETER), PSTR("0.80"), lcd_nozzle_diameter_cycle); break;\
+        case ClNozzleDiameter::_Diameter_1000: MENU_ITEM_TOGGLE_P(_T(MSG_NOZZLE_DIAMETER), PSTR("1.00"), lcd_nozzle_diameter_cycle); break;\
+        case ClNozzleDiameter::_Diameter_1200: MENU_ITEM_TOGGLE_P(_T(MSG_NOZZLE_DIAMETER), PSTR("1.20"), lcd_nozzle_diameter_cycle); break;\
+        case ClNozzleDiameter::_Diameter_1400: MENU_ITEM_TOGGLE_P(_T(MSG_NOZZLE_DIAMETER), PSTR("1.40"), lcd_nozzle_diameter_cycle); break;\
     }\
 }\
 while (0)
